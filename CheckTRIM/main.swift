65d884fdef2e34940adfa94960a2d1c43a217940
Dmitry Remesov <dmitry@remesov.ru>
2014-11-29 00:03:53 +0300

    Initial Commit

---
+++ CheckTRIM/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CheckTRIM
+//
+//  Created by Dmitry Remesov on 29.11.14.
+//  Copyright (c) 2014 Dmitry Remesov. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
